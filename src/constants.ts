4cacd775d38f30f82b93f95330615c01de653a09
Vladimir Gulev <vladinson009@icloud.com>
2025-06-24 19:10:50 +0200

    fixed constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 // * Mongoose schema
-export const HASH_ROUNDS = 12;
+export const HASH_ROUNDS = process.env.HASH_ROUNDS || 8;
 export const MIN_USERNAME_LENGTH = 3;
 export const MIN_EMAIL_LENGTH = 6;
 export const MIN_PASSWORD_LENGTH = 6;
