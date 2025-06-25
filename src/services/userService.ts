e8865e9a9bf723fa8fdaebb82f0958ad5432d2a3
Vladimir Gulev <vladinson009@icloud.com>
2025-06-26 00:21:42 +0200

    removed error handler for services and added global one

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,39 +1,35 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
+
 import createToken from '../utils/createToken.js';
 import { LoginUserSchema } from '../utils/validateLoginUser.js';
 import {
   RegisterUserInput,
   RegisterUserSchema,
 } from '../utils/validateRegisterUser.js';
+import { HttpError } from '../utils/errorParser.js';
+import { UserId } from '../types/UserService.js';
 
 export default class UserService {
   public static async register(userInput: unknown) {
     const parsed = await RegisterUserSchema.safeParseAsync(userInput);
     if (!parsed.success) {
-      throw new Error(parsed.error.issues.map((i) => i.message).join(', '));
+      throw new HttpError(400, parsed.error.issues.map((i) => i.message).join(', '));
     }
     const { repass, ...validData }: RegisterUserInput = parsed.data;
-    try {
-      const isUser = await User.findOne({
-        $or: [{ email: validData.email }, { username: validData.username }],
-      });
-      if (isUser) {
-        if (isUser.email === validData.email) {
-          throw new Error('Email already exists!');
-        } else if (isUser.username === validData.username) {
-          throw new Error('Username already exists!');
-        }
-      }
-      const newUser = await User.create(validData);
-      return createToken(newUser);
-    } catch (error) {
-      if (error instanceof Error) {
-        throw error.message;
-      } else {
-        throw error;
+
+    const isUser = await User.findOne({
+      $or: [{ email: validData.email }, { username: validData.username }],
+    });
+    if (isUser) {
+      if (isUser.email === validData.email) {
+        throw new HttpError(409, 'Email already exists!');
+      } else if (isUser.username === validData.username) {
+        throw new HttpError(409, 'Username already exists!');
       }
     }
+    const newUser = await User.create(validData);
+    return createToken(newUser);
   }
   public static async login(userInput: unknown) {
     if (
@@ -44,33 +40,27 @@ export default class UserService {
       typeof userInput.password !== 'string' ||
       typeof userInput.username !== 'string'
     ) {
-      throw new Error('Invalid User Input Data');
+      throw new HttpError(400, 'Invalid User Input Data');
     }
-    try {
-      const parsed = await LoginUserSchema.safeParseAsync(userInput);
+    const parsed = await LoginUserSchema.safeParseAsync(userInput);
 
-      if (!parsed.success) {
-        throw new Error(parsed.error.issues.map((i) => i.message).join(', '));
-      }
-      const validData = parsed.data;
-      const user = await User.findOne({ username: validData.username });
-      if (!user) {
-        throw new Error('Invalid email or password');
-      }
-      const userInputPassword = userInput.password;
-      const userPassword = user.password;
-      const isValidPassword = await bcrypt.compare(userInputPassword, userPassword);
-
-      if (!isValidPassword) {
-        throw new Error('Invalid email or password');
-      }
-      return createToken(user);
-    } catch (error) {
-      if (error instanceof Error) {
-        throw error.message;
-      } else {
-        throw error;
-      }
+    if (!parsed.success) {
+      throw new HttpError(400, parsed.error.issues.map((i) => i.message).join(', '));
     }
+    const { username, password } = parsed.data;
+    const user = await User.findOne({ username });
+    if (!user) {
+      throw new HttpError(401, 'Invalid email or password');
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      throw new HttpError(401, 'Invalid email or password');
+    }
+    return createToken(user);
+  }
+  public static async logout(userId: UserId): Promise<Response | null> {
+    return User.findByIdAndUpdate(userId, { $inc: { tokenVersion: 1 } });
   }
 }
