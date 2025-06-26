94ae7773cb40e8706319363495feecf3fd558914
Vladimir Gulev <vladinson009@icloud.com>
2025-06-26 21:26:31 +0200

    added nested routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 
 import userController from './controllers/userController.js';
+import { usersPath } from './constants/routeConstants.js';
 
 const router = Router();
 
-router.use(userController);
+router.use(usersPath.root, userController);
 
 export default router;
