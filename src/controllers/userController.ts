4eae910c14c6bea93b7b914b273e87465236607c
Vladimir Gulev <vladinson009@icloud.com>
2025-06-26 21:27:39 +0200

    try catch clause for logout

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,4 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 import UserService from '../services/userService.js';
 import { authenticate } from '../middlewares/authenticate.js';
@@ -9,7 +9,7 @@ const userController = Router();
 // * Register new user
 userController.post(
   usersPath.register,
-  async (req: Request, res: Response, next) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const userInput: unknown = req.body;
     try {
       const token = await UserService.register(userInput);
@@ -20,28 +20,34 @@ userController.post(
   }
 );
 // * Login user
-userController.post(usersPath.login, async (req: Request, res: Response, next) => {
-  const userInput: unknown = req.body;
-  try {
-    const token = await UserService.login(userInput);
-    res.json(token);
-  } catch (error) {
-    next(error);
+userController.post(
+  usersPath.login,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userInput: unknown = req.body;
+    try {
+      const token = await UserService.login(userInput);
+      res.json(token);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 // * Logout user
 userController.post(
   usersPath.logout,
   authenticate,
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = req.user;
     if (!user) {
       res.status(401).json({ message: 'Unauthorized' });
       return;
     }
-
-    await UserService.logout(user._id);
-    res.status(200).json({ message: 'Logged out successfully' });
+    try {
+      await UserService.logout(user._id);
+      res.status(200).json({ message: 'Logged out successfully' });
+    } catch (error) {
+      next(error);
+    }
   }
 );
 export default userController;
