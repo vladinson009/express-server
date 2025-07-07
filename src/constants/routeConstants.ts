08c208211c284285903f1288aa3321ac272c423a
Vladimir Gulev <vladinson009@icloud.com>
2025-07-07 22:20:36 +0200

    added cards path

--- src/constants/routeConstants.ts
+++ src/constants/routeConstants.ts
@@ -5,3 +5,10 @@ export const usersPath = {
   logout: '/logout',
   me: '/me',
 };
+export const cardsPath = {
+  root: '/cards',
+  create: '/create',
+  edit: '/edit/:cardId',
+  delete: '/delete/:cardId',
+  getById: '/:cardId',
+};
