07b63f1df4ed9a14a1dceb76810416418b227cee
Vladimir Gulev <vladinson009@icloud.com>
2025-07-07 22:23:01 +0200

    implemented card model

---
+++ src/models/Card.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose';
+import { cardConstants } from '../constants/cardConstants.js';
+
+const cardSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: cardConstants.MIN_TITLE_LENGTH,
+    },
+    description: {
+      type: String,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+      minLength: cardConstants.MIN_IMG_URL_LENGTH,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default model('card', cardSchema);
