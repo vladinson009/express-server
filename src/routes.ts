9853e8df89d5775e301b4e79a60826c9b5d4cc88
Vladimir Gulev <vladinson009@icloud.com>
2025-07-07 22:20:02 +0200

    Cards collection routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 
 import userController from './controllers/userController.js';
-import { usersPath } from './constants/routeConstants.js';
+import { cardsPath, usersPath } from './constants/routeConstants.js';
+import cardController from './controllers/cardController.js';
 
 const router = Router();
 
 router.use(usersPath.root, userController);
+router.use(cardsPath.root, cardController);
 
 export default router;
