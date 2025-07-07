06bb8038a684940922e61f23793a54a1f1fc7843
Vladimir Gulev <vladinson009@icloud.com>
2025-07-07 22:20:27 +0200

    changed db string

--- src/constants/environment.ts
+++ src/constants/environment.ts
@@ -2,13 +2,13 @@ import { Environment, EnvironmentConfigs } from '../types/Constants';
 
 const configs: EnvironmentConfigs = {
   development: {
-    DB_CONNECTION_STRING: 'mongodb://localhost:27017/dog-hills',
+    DB_CONNECTION_STRING: 'mongodb://localhost:27017/collection',
     SECRET_TOKEN: 'alaBaLa',
     HASH_ROUNDS: 8,
   },
   production: {
     DB_CONNECTION_STRING:
-      process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/dog-hills',
+      process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/collection',
     SECRET_TOKEN: process.env.SECRET_TOKEN || 'alaBaLa',
     HASH_ROUNDS: Number(process.env.HASH_ROUNDS) || 8,
   },
