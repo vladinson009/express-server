518d8d358f2c81a52a4621cfb2e0d279b287139e
Vladimir Gulev <vladinson009@icloud.com>
2025-07-13 04:13:14 +0200

    implemented routes for games, categories, platforms

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,24 @@
 import { Router } from 'express';
+import {
+  cardsPath,
+  usersPath,
+  gamesPath,
+  categoriesPath,
+  platformsPath,
+} from './constants/routeConstants.js';
 
 import userController from './controllers/userController.js';
-import { cardsPath, usersPath } from './constants/routeConstants.js';
 import cardController from './controllers/cardController.js';
+import gameController from './controllers/gameController.js';
+import categoryController from './controllers/categoryController.js';
+import platformController from './controllers/platformController.js';
 
 const router = Router();
 
 router.use(usersPath.root, userController);
 router.use(cardsPath.root, cardController);
+router.use(gamesPath.root, gameController);
+router.use(categoriesPath.root, categoryController);
+router.use(platformsPath.root, platformController);
 
 export default router;
