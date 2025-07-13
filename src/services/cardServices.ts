c68af67cce1b950f3dfb1505493dfc332f825ef6
Vladimir Gulev <vladinson009@icloud.com>
2025-07-13 04:16:27 +0200

    implemented services from abstract class

--- src/services/cardServices.ts
+++ src/services/cardServices.ts
@@ -5,35 +5,3 @@ import { CreateCardSchema } from '../utils/validators/validateCreateCard.js';
 import { BaseService } from './baseService.js';
 
 export default new BaseService(Card, CreateCardSchema, EditCardSchema);
-// export default class CardServices {
-//   public static getById(cardId: string) {
-//     return Card.findById(cardId);
-//   }
-//   public static getAll() {
-//     return Card.find();
-//   }
-//   public static async createCard(cardInput: unknown) {
-//     const parsed = await CreateCardSchema.safeParseAsync(cardInput);
-//     if (!parsed.success) {
-//       throw new HttpError(400, parsed.error.issues.map((i) => i.message).join(', '));
-//     }
-//     const parsedData = parsed.data;
-//     const newCard = Card.create(parsedData);
-//     return newCard;
-//   }
-//   public static async editCard(cardInput: unknown, cardId: string) {
-//     const parsed = await EditCardSchema.safeParseAsync(cardInput);
-//     if (!parsed.success) {
-//       throw new HttpError(400, parsed.error.issues.map((i) => i.message).join(', '));
-//     }
-//     const parsedData = parsed.data;
-//     const updatedCard = Card.findByIdAndUpdate(cardId, parsedData, {
-//       new: true,
-//       runValidators: true,
-//     });
-//     return updatedCard;
-//   }
-//   public static async deleteCard(cardId: string) {
-//     return Card.findByIdAndDelete(cardId);
-//   }
-// }
--- src/services/categoryServices.ts
+++ src/services/categoryServices.ts
@@ -2,38 +2,7 @@
 import Category from '../models/Category.js';
 
 import { CreateCategorySchema } from '../utils/validators/validateCreateCategory.js';
+import { EditCategorySchema } from '../utils/validators/validateEditCategory.js';
 import { BaseService } from './baseService.js';
 
-export default new BaseService(Category, CreateCategorySchema);
-// export default class CategoryServices {
-//   public static getById(categoryId: string) {
-//     return Category.findById(categoryId);
-//   }
-//   public static getAll() {
-//     return Category.find();
-//   }
-//   public static async createCategory(categoryInput: unknown) {
-//     const parsed = await CreateCategorySchema.safeParseAsync(categoryInput);
-//     if (!parsed.success) {
-//       throw new HttpError(400, parsed.error.issues.map((i) => i.message).join(', '));
-//     }
-//     const parsedData = parsed.data;
-//     const newCategory = Category.create(parsedData);
-//     return newCategory;
-//   }
-//   public static async editCategory(categoryInput: unknown, categoryId: string) {
-//     const parsed = await CreateCategorySchema.safeParseAsync(categoryInput);
-//     if (!parsed.success) {
-//       throw new HttpError(400, parsed.error.issues.map((i) => i.message).join(', '));
-//     }
-//     const parsedData = parsed.data;
-//     const updatedCategory = Category.findByIdAndUpdate(categoryId, parsedData, {
-//       new: true,
-//       runValidators: true,
-//     });
-//     return updatedCategory;
-//   }
-//   public static async deleteCategory(categoryId: string) {
-//     return Category.findByIdAndDelete(categoryId);
-//   }
-// }
+export default new BaseService(Category, CreateCategorySchema, EditCategorySchema);
