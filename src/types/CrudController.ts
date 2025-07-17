9b94e1494f5c6a18ec0c4cff033aec4c5f09d174
Vladimir Gulev <vladinson009@icloud.com>
2025-07-17 23:28:43 +0200

    added types

--- src/types/CrudController.ts
+++ src/types/CrudController.ts
@@ -5,13 +5,17 @@ export type CrudControllerConfig = {
     create: string;
     edit: string;
     delete: string;
+    like?: string;
+    unlike?: string;
   };
   paramName: 'cardId' | 'gameId' | 'platformId' | 'categoryId'; // e.g. 'cardId' or 'gameId'
   service: {
     getById: (id: string) => Promise<any>;
-    getAll: () => Promise<any>;
+    getAll: (query: Record<string, any>) => Promise<any>;
     create: (input: unknown) => Promise<any>;
     edit: (input: unknown, id: string) => Promise<any>;
+    addLike: (id: string, userId: string) => Promise<any>;
+    removeLike: (id: string, userId: string) => Promise<any>;
     delete: (id: string) => Promise<any>;
   };
 };
