d995ac92d0ab850e9ad952dbe948fce16a1c9326
Vladimir Gulev <vladinson009@icloud.com>
2025-07-17 23:25:24 +0200

    implemented push/pull likes & added query params

--- src/controllers/abstractCrudController.ts
+++ src/controllers/abstractCrudController.ts
@@ -25,7 +25,7 @@ export function createCrudController({
   // GET ALL
   router.get(paths.getAll, async (req, res, next) => {
     try {
-      const result = await service.getAll();
+      const result = await service.getAll(req.query);
       res.status(200).json(result);
     } catch (error) {
       next(error);
@@ -49,8 +49,11 @@ export function createCrudController({
       const userId = req.user!._id;
 
       const doc = await service.getById(id);
-      const author = doc?.author;
-      if (!author?.equals(userId)) {
+      if (!doc) {
+        throw new HttpError(404, 'Document does not exists!');
+      }
+      const author = doc.author;
+      if (!author._id?.equals(userId)) {
         throw new HttpError(401, 'You are not the author!');
       }
 
@@ -68,13 +71,38 @@ export function createCrudController({
       const userId = req.user!._id;
 
       const doc = await service.getById(id);
+      if (!doc) {
+        throw new HttpError(404, 'Document does not exists!');
+      }
       const author = doc?.author;
-      if (!author?.equals(userId)) {
+      if (!author?._id.equals(userId)) {
         throw new HttpError(401, 'You are not the author!');
       }
 
       const deleted = await service.delete(id);
-      res.status(204).json(deleted);
+      res.status(200).json(deleted);
+    } catch (error) {
+      next(error);
+    }
+  });
+  // PUSH LIKE TO SET
+  router.get(paths.like!, authenticate, async (req, res, next) => {
+    try {
+      const id = req.params[paramName];
+      const userId = req.user!._id.toString();
+      const liked = await service.addLike(id, userId);
+      res.status(200).json(liked);
+    } catch (error) {
+      next(error);
+    }
+  });
+  // PULL LIKE FROM SET
+  router.get(paths.unlike!, authenticate, async (req, res, next) => {
+    try {
+      const id = req.params[paramName];
+      const userId = req.user!._id.toString();
+      const unliked = await service.removeLike(id, userId);
+      res.status(200).json(unliked);
     } catch (error) {
       next(error);
     }
