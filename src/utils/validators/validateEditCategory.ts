57ba311619cebce909f850a72cca663fe6ccc87a
Vladimir Gulev <vladinson009@icloud.com>
2025-07-30 00:41:26 +0300

    validate edit bug fixed

--- src/utils/validators/validateEditCategory.ts
+++ src/utils/validators/validateEditCategory.ts
@@ -1,6 +1,9 @@
 import { z } from 'zod';
 
 export const EditCategorySchema = z.object({
+  name: z
+    .string({ required_error: 'Name is required!' })
+    .nonempty('Name is required!'),
   description: z.string().optional(),
 });
 
